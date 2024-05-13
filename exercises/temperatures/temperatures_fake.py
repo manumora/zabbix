5a9a2f4759ea478fdf28d2f701b40eb11c70284e
Manu <manuel@fusiontelecom.co>
2024-05-13 09:19:14 +0200

    Added plugins

--- exercises/temperatures/temperatures_fake.py
+++ exercises/temperatures/temperatures_fake.py
@@ -1,2 +1,2 @@
 #!/usr/bin/env python3
-print ({"acpitz - 0": 27.8, "iwlwifi_1 - 0": 37.0, "coretemp - Package id 0": 33.0, "coretemp - Core 0": 33.0, "coretemp - Core 1": 29.0, "coretemp - Core 2": 30.0, "coretemp - Core 3": 29.0})
+print ('{"acpitz - 0": 27.8, "iwlwifi_1 - 0": 37.0, "coretemp - Package id 0": 33.0, "coretemp - Core 0": 33.0, "coretemp - Core 1": 29.0, "coretemp - Core 2": 30.0, "coretemp - Core 3": 29.0}')
